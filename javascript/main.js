3a9d69bc3c14e7ea1a40ec3d001f587f238cba96
Delight Solution <delight@Delights-MacBook-Pro.local>
2023-03-15 11:40:00 +0700

    responsive actions menu

--- javascript/main.js
+++ javascript/main.js
@@ -1,11 +1,13 @@
 var open = document.getElementById("open-icon");
 var close = document.getElementById("close-icon");
 var menus = document.getElementById("menu");
+var actions = document.getElementById("actions");
 
 open.addEventListener("click", (e) => {
   open.style.display = "none";
   close.style.display = "inline-block";
   menus.classList.add("open");
+  actions.style.display = "flex";
   document.body.style.overflow = "hidden";
 });
 
@@ -13,5 +15,6 @@ close.addEventListener("click", (e) => {
   open.style.display = "inline-block";
   close.style.display = "none";
   menus.classList.remove("open");
+  actions.style.display = "none";
   document.body.style.overflow = "scroll";
 });
